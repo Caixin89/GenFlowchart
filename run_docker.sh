f0a95c3e01d43a449dd3e190955a6320377118ba
Chai Zheng Xin <caixin89@gmail.com>
2025-10-02 15:15:22 +0000

    Updates

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,9 @@ docker run \
     -d \
     -v "$PWD/Patent Images":"/home/zhengxin.chai/GenFlowchart/Patent Images" \
     -v "$PWD/patent pdfs":"/home/zhengxin.chai/GenFlowchart/patent pdfs" \
+    -v "$PWD/Flowchart Data.xlsx":"/home/zhengxin.chai/GenFlowchart/Flowchart Data.xlsx" \
     -v $PWD/GenFlowchart.ipynb:/home/zhengxin.chai/GenFlowchart/GenFlowchart.ipynb \
+    -v $PWD/outputs:/home/zhengxin.chai/GenFlowchart/outputs \
     -p 8888:8888 \
     --gpus all \
     genflowchart
