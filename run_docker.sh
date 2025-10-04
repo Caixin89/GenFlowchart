1892ee334841eff849338f1061430863a3378fce
Chai Zheng Xin <caixin89@gmail.com>
2025-10-04 11:56:58 +0800

    Updates

--- run_docker.sh
+++ run_docker.sh
@@ -3,6 +3,7 @@
 docker run \
     --rm \
     -d \
+    --env-file .env \
     -v "$PWD/Patent Images":"/home/zhengxin.chai/GenFlowchart/Patent Images" \
     -v "$PWD/patent pdfs":"/home/zhengxin.chai/GenFlowchart/patent pdfs" \
     -v "$PWD/Flowchart Data.xlsx":"/home/zhengxin.chai/GenFlowchart/Flowchart Data.xlsx" \
