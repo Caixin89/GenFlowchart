10fbe20006fc9d245bec17d65c9bf8dbb8202b4d
Chai Zheng Xin <caixin89@gmail.com>
2025-12-30 21:26:27 +0800

    Move to notebooks folder

--- run_docker.sh
+++ run_docker.sh
@@ -6,7 +6,7 @@ docker run \
     -v "$PWD/Patent Images":"/home/zhengxin.chai/GenFlowchart/Patent Images" \
     -v "$PWD/patent pdfs":"/home/zhengxin.chai/GenFlowchart/patent pdfs" \
     -v "$PWD/Flowchart Data.xlsx":"/home/zhengxin.chai/GenFlowchart/Flowchart Data.xlsx" \
-    -v $PWD/GenFlowchart.ipynb:/home/zhengxin.chai/GenFlowchart/GenFlowchart.ipynb \
+    -v $PWD/notebooks:/home/zhengxin.chai/GenFlowchart/notebooks \
     -v $PWD/outputs:/home/zhengxin.chai/GenFlowchart/outputs \
     -p 8888:8888 \
     genflowchart-cpu
